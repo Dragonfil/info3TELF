bffe8b2862ac1df75c85f6285691535231d847ae
Dragonfil 72 <filippo.rossi@studenti.malignani.ud.it>
2024-05-27 08:36:58 +0000

    prove in classe

---
+++ Progetti c++/progetto27.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Benvenuto su GITPOD" << endl;
+    string name;
+    cout << "Scrivi il tuo nome: ";
+    cin >> name;
+    cout << "Ciao " << name;
+    return 0;
+}
